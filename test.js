0b3bdf30ac3b05a79f8e850211b9e103b23f9823
softiet <howdie11002@gmail.ccom>
2019-01-18 19:09:44 +0800

    as

---
+++ test.js
@@ -0,0 +1,9 @@
+function menu_click(){
+  if(document.getElementById('nav_content').style.display != "none"){
+    document.getElementById('nav_content').style.display = "none";
+  }
+  else{
+    document.getElementById('nav_content').style.display = "block";
+  }
+
+}
